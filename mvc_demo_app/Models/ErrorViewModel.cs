23fe615c1c26d2fbeed1a963a4ab1622d5487871
Seishin LeBlanc <seishinleblanc@gmail.com>
2023-10-22 09:59:59 -0500

    First commit.

---
+++ mvc_demo_app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace mvc_demo_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
